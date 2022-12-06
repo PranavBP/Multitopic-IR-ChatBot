f910a7f5378019a1ade2ecb38e275d71e1af6506
PranavBP <bellam.pranav@gmail.com>
2022-12-06 23:21:50 +0000

    added tensorflow model for data

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from chatterbot import ChatBot
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 import logging
+import tensorflow as tf
+import tensorflow_text as text
+import numpy as np
+from sentence_transformers import SentenceTransformer, util
 
 CB = ChatBot('ChatBot')
 app = Flask(__name__)
@@ -9,6 +13,41 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 logging.getLogger('flask_cors').level = logging.DEBUG
+print('start model_simi')
+model_simi = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
+print('start model..done simi')
+model = tf.keras.models.load_model("/home/bellam_pranav/classmodelnew")
+print("done model")
+print(model.summary())
+
+def execute(query):
+    coreidnp = model.predict([query])
+
+    print(query,coreidnp)
+    coreidnp = np.where(coreidnp > 0.25, 1, 0)
+    coreid = coreidnp[0][0]
+    print(coreid)
+    if coreid == 0:
+        core = 'chitchat'
+    else:
+        core = 'reddit'
+
+    search_url = ''
+
+
+    retrieved = ['third wave of covid?','vaccine for covid are back is stock?']
+    query_embedding = model_simi.encode(query)
+    
+    ret_embedding = model_simi.encode(retrieved)
+    similarity = util.dot_score(query_embedding, ret_embedding)
+
+    simi = similarity.numpy()
+    max_sim_index = np.argmax(simi)
+    max_sim = np.amax(simi)
+    print("Similarity:", similarity, max_sim,max_sim_index)
+
+    top_reply = retrieved[max_sim_index]
+    return top_reply
 
 @app.route("/")
 def helloWorld():
@@ -24,8 +63,9 @@ def home():
 def getInput():
     try:
         data = request.get_json()
-        print(data["query"])
-        return "bouis played wewdkc"
+        result = execute(data["query"])
+        print(result)
+        return result
     except Exception as e:
         print(e)
         return "Bhai kya kar rha hai thu? :/"
@@ -36,4 +76,4 @@ def get_bot_response():
     return str(CB.get_response(userText))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True,port=9999)
+    app.run(host='0.0.0.0',debug=False,port=9999)
